2d9ede5c6a8ba9088d17816946fffa457eba5cf7
Wim Vanhenden <wim.vanhenden@gmail.com>
2024-05-08 15:35:50 +0200

    added start scripts

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo START RUNNING CAPTURE
-start "capture" cmd /k "conda activate david && python capture.py"
-
-timeout /t 15
-
-echo START RUNNING NARRATOR
-start "capture" cmd /k "conda activate david &&set OPENAI_API_KEY=put_key_here&&set ELEVENLABS_API_KEY=put_key_here&&set ELEVENLABS_VOICE_ID=put_key_here&& python narrator.py"
---
+++ start_capture.bat
@@ -0,0 +1,1 @@
+conda activate david && python capture.py

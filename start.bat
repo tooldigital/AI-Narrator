ab56dccda2bfeac78554a6f1fb0a9bbf9f87ce39
Wim Vanhenden <wim.vanhenden@gmail.com>
2024-05-08 12:10:06 +0200

    changed keys

--- start.bat
+++ start.bat
@@ -5,4 +5,4 @@ start "capture" cmd /k "conda activate david && python capture.py"
 timeout /t 15
 
 echo START RUNNING NARRATOR
-start "capture" cmd /k "conda activate david &&set OPENAI_API_KEY=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&&set ELEVENLABS_API_KEY=b36a456cc2707104415c314ce5eeac0f&&set ELEVENLABS_VOICE_ID=0UxdNWAQ8l9YJ71sFabP&& python narrator.py"
+start "capture" cmd /k "conda activate david &&set OPENAI_API_KEY=put_key_here&&set ELEVENLABS_API_KEY=put_key_here&&set ELEVENLABS_VOICE_ID=put_key_here&& python narrator.py"

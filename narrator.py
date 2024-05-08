f9c7c0d1c598679fe03f9166cf715a0f30af79bb
Wim Vanhenden <wim.vanhenden@gmail.com>
2024-05-08 15:36:52 +0200

    added adidas logo recognition

--- narrator.py
+++ narrator.py
@@ -6,10 +6,31 @@ import time
 import simpleaudio as sa
 import errno
 from elevenlabs import generate, play, set_api_key, voices
+from ultralytics import YOLO
+
+model = YOLO('./adidas_yolov8_m_e25.pt')
 
 client = OpenAI()
 
 set_api_key(os.environ.get("ELEVENLABS_API_KEY"))
+
+
+def check_for_adidas(image_path):
+    while True:
+        try:
+            results = model(image_path, conf=0.3, imgsz=640, verbose=False)
+            if(len(results[0].boxes)>0):
+                return True
+            else:
+                return False
+
+        except IOError as e:
+            if e.errno != errno.EACCES:
+                # Not a "file in use" error, re-raise
+                raise
+            # File is being written to, wait a bit and retry
+            time.sleep(0.1)
+
 
 def encode_image(image_path):
     while True:
@@ -54,22 +75,31 @@ def generate_new_line(base64_image):
     ]
 
 
-def analyze_image(base64_image, script):
+def analyze_image(base64_image, script, isadidas=False):
+
+    systemrole = """
+                You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary.
+                Make it snarky and funny. Don't repeat yourself. Make it short maximum two sentences. If you see anything remotely interesting or you see differences with previous pictures, make a big deal about it!
+                """
+    if isadidas==True:
+        systemrole = """
+                You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary.
+                Make it snarky and funny. Make it short maximum two sentences. An Adidas logo has been detected. Elaborate on that. See if the item that has the logo on it is some piece of clothing. Make a big deal about it!
+                """
+
     response = client.chat.completions.create(
         model="gpt-4-vision-preview",
         messages=[
             {
                 "role": "system",
-                "content": """
-                You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary.
-                Make it snarky and funny. Don't repeat yourself. Make it short. If I do anything remotely interesting, make a big deal about it!
-                """,
+                "content": systemrole,
             },
         ]
         + script
         + generate_new_line(base64_image),
         max_tokens=500,
-    )
+        )
+    
     response_text = response.choices[0].message.content
     return response_text
 
@@ -81,12 +111,16 @@ def main():
         # path to your image
         image_path = os.path.join(os.getcwd(), "./frames/frame.jpg")
 
+        #check for signs of Adidas
+        adidasfound = check_for_adidas(image_path)
+        print("ADIDAS FOUND "+str(adidasfound))
+
         # getting the base64 encoding
         base64_image = encode_image(image_path)
 
         # analyze posture
         print("👀 David is watching...")
-        analysis = analyze_image(base64_image, script=script)
+        analysis = analyze_image(base64_image, script=script, isadidas=adidasfound)
 
         print("🎙️ David says:")
         print(analysis)
@@ -94,6 +128,9 @@ def main():
         play_audio(analysis)
 
         script = script + [{"role": "assistant", "content": analysis}]
+
+        #script =  [{"role": "assistant", "content": analysis}]
+
 
         # wait for 5 seconds
         time.sleep(5)

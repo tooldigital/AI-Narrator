4dafc83aea21f978aa1217f1f8c56fcdb994cf36
ramon <juan.ramon93@hotmail.com>
2024-05-13 16:23:31 -0700

    updated Audio generation to wait for next audio

--- frontend/src/components/David/DavidRenderer.jsx
+++ frontend/src/components/David/DavidRenderer.jsx
@@ -3,19 +3,31 @@ import david from 'assets/images/david-front.png';
 import davidGlare from 'assets/images/david-glass-glare.png';
 import { useEffect, useRef, useState } from 'react';
 import {get_elevenlabs_audio, play_audio, request_new_script} from 'src/js/narrator';
+
+const pregenTiming = 3;
 const DavidRenderer = () => {
+
     const [dialog, setDialog] = useState("Hi I am david");
     const [dialogAnimated, setDialogAnimated] = useState("Hi I am david");
     const [screenShot, setScreenShot] = useState(null);
     const [startedLooking, setStartedLooking] = useState(false);
     const [isLooking, setIsLooking] = useState(false);
     const [index, setIndex] = useState(0);
+    const [audioSrc, setAudioSrc] = useState(null);
+    const [canPlayAudio, setCanPlayAudio] = useState(false);
+    const [newScriptReady, setNewScriptReady] = useState(false);
     const intervalRef = useRef(null);
     const imageTestRef = useRef(null);
     const screenshotRef = useRef(null);
     const videoLeftRef = useRef(null);
-    const videoRightRef = useRef(null); 
+    const videoRightRef = useRef(null);
+
     const audio = useRef(null);
+    const nextAudioSrc = useRef(null);
+    const canGenerateAudio = useRef(true);
+    
+    const isFirstTime = useRef(true);
+
     const renderDialog = () =>{
         let newSubString = dialog.substring(0, index + 1);
         setDialogAnimated(newSubString);
@@ -59,9 +71,18 @@ const DavidRenderer = () => {
         // request_new_script(imageTestRef.current.src, true);
         setScreenShot(dataUrl);
     }
+
+    const generateAudioScript = async () => {
+        let newText = await request_new_script(screenShot, true);
+        setDialogAnimated(newText);
+        console.log(newText);
+        let  _audioSrc= await get_elevenlabs_audio(newText)
+        // setAudioSrc(_audioSrc);
+        return _audioSrc;
+    }
     
     const invokeGPTAnalysis =  async () => {
-        console.log(screenShot);
+        // console.log(screenShot);
         if(screenShot){
             if(audio.current){
                 console.log(audio.current.ended, audio.current.paused);
@@ -69,21 +90,42 @@ const DavidRenderer = () => {
                     return;
                 }
             }
-            let newText = await request_new_script(screenShot, true);
-            setDialogAnimated(newText);
-            let audioSrc = await get_elevenlabs_audio(newText)
+            
             if(!audio.current){
-                audio.current = new Audio(audioSrc);
+                nextAudioSrc.current =  await generateAudioScript();
+                // console.log("currentAudioSrc", nextAudioSrc.current);
+                audio.current = new Audio(nextAudioSrc.current);
                 
-                audio.current.onended = (event) => {
-                    console.log("audio ended");
-                    setStartedLooking(false);
+                audio.current.ontimeupdate = async (event) => {
+                    if(canGenerateAudio.current){
+                        if(audio.current.duration - audio.current.currentTime < pregenTiming){
+                            console.log("===generaring new script===");
+                            canGenerateAudio.current = false;
+                            setCanPlayAudio(false);
+                            setNewScriptReady(false);
+                            nextAudioSrc.current =  await generateAudioScript();
+                            setNewScriptReady(true);
+
+                            // setAudioSrc(_audioSrc);
+                            console.log("===done generating new script===");
+                            // invokeGPTAnalysis();
+                        }
+                        // shouldGenerateAudio.current = false;
+                        // setStartedLooking(false);
+                        // invokeGPTAnalysis();
+                    }
                 };
+                audio.current.onended = (event) => {
+                    // console.log("audio ended");
+                    setCanPlayAudio(true);
+                    // setStartedLooking(false);
+                };
+                audio.current.play(); 
             }else{
                 audio.current.pause();
-                audio.current.src = audioSrc;
-            }
-            audio.current.play();       
+                audio.current.src = nextAudioSrc.current;
+                audio.current.play(); 
+            }      
             // console.log(audio);
         }
     }
@@ -99,12 +141,40 @@ const DavidRenderer = () => {
             setIsLooking(false);
         }
     }
+    useEffect(() => {
+        if(!isLooking){
+            isFirstTime.current = true
+        }
+    }, [isLooking]);
+
+    useEffect(() => {
+        if(newScriptReady){
+            setAudioSrc(nextAudioSrc.current)
+        }
+    }, [newScriptReady]);
+
+    useEffect(() => {
+        
+        if(isLooking){
+            // console.log("//--------audio update--------//");
+            // console.log("condition check:", canPlayAudio, newScriptReady);
+            if(canPlayAudio && newScriptReady){
+                audio.current.src = audioSrc;
+                audio.current.play(); 
+                canGenerateAudio.current = true;
+                // console.log("//--------success--------//");
+
+            }else{
+                // console.log("//--------Fail--------//");
+
+            }
+        }
+    }, [canPlayAudio,audioSrc]);
 
     useEffect(() => {
         if(screenShot){
-            console.log("startedLooking", startedLooking);
-            if(!startedLooking){
-                setStartedLooking(true);
+            if(isFirstTime.current){
+                isFirstTime.current = false;
                 invokeGPTAnalysis();
             }
         }
